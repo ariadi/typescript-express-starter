42fa885db03ef001abb5e3bf4ed5cefe53460dfd
ariadi <me@ariadi.net>
2024-05-26 15:27:52 +0700

    Update users.dto.ts
    Small refactoring

--- lib/prisma/src/dtos/users.dto.ts
+++ lib/prisma/src/dtos/users.dto.ts
@@ -1,20 +1,15 @@
-import { IsEmail, IsString, IsNotEmpty, MinLength, MaxLength } from 'class-validator';
+import { IsEmail, IsString, IsNotEmpty, Length } from 'class-validator';
 
-export class CreateUserDto {
+class BaseUserDto {
+  @IsString()
+  @IsNotEmpty()
+  @Length(9,32)
+  public password: string;
+}
+
+export class CreateUserDto extends BaseUserDto {
   @IsEmail()
   public email: string;
-
-  @IsString()
-  @IsNotEmpty()
-  @MinLength(9)
-  @MaxLength(32)
-  public password: string;
 }
 
-export class UpdateUserDto {
-  @IsString()
-  @IsNotEmpty()
-  @MinLength(9)
-  @MaxLength(32)
-  public password: string;
-}
+export class UpdateUserDto extends BaseUserDto {}
